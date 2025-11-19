ed03721391376fcd6c1109f136cbeaadc6e31b82
bpikaar <bobpikaar@gmail.com>
2025-11-19 11:25:40 +0100

    ADD lesson 4

---
+++ les4/examples/validation/index.php
@@ -0,0 +1,43 @@
+<?php
+// standaard is er geen error
+$emailError = '';
+
+// check op Postback
+if(isset($_POST['submit'])) {
+
+    // Als er niks is ingevuld in het inputveld
+    if($_POST['email'] == '') {
+        // Maak een error message
+        $emailError = 'Het e-mailadres moet ingevuld zijn.';
+    }
+
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+    <title>Validation page 1</title>
+</head>
+<body class="container">
+
+    <section class="section">
+        <form action="" method="post">
+
+            <label for="email">Email</label>
+            <input id="email" type="text" name="email" />
+
+            <!--Toon hier de error. Als er geen error is, wordt de span gevuld met een lege string. Deze zie je dan niet in de website -->
+            <span><?= $emailError ?></span>
+
+            <input type="submit" name="submit" />
+
+        </form>
+    </section>
+
+</body>
+</html>
