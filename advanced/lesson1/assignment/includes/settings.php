4fb4c9156d24fca2f28306da4dd1df3614ac038a
antwanvdm <antwanvdm@gmail.com>
2025-11-26 12:05:31 +0100

    Assignment advanced lesson 1

---
+++ advanced/lesson1/assignment/includes/settings.php
@@ -0,0 +1,8 @@
+<?php
+//Define custom constants to use with project
+const DATA_PATH = __DIR__ . '/data/';
+
+//Custom error handler, so every error will throw a custom ErrorException
+set_error_handler(function ($severity, $message, $file, $line) {
+    throw new ErrorException($message, $severity, $severity, $file, $line);
+});
